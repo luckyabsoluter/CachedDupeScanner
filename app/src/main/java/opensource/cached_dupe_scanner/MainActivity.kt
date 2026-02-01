45f5939d6906a945f8ff696ec8f865595fadf9d2
luckyabsoluter <80892490+luckyabsoluter@users.noreply.github.com>
2026-02-01 13:06:52 +0900

    style(ui): apply system bars padding in compose

--- app/src/main/java/opensource/cached_dupe_scanner/MainActivity.kt
+++ app/src/main/java/opensource/cached_dupe_scanner/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import opensource.cached_dupe_scanner.ui.home.HomeScreen
@@ -17,7 +19,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             CachedDupeScannerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen(modifier = Modifier.fillMaxSize())
+                    HomeScreen(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .systemBarsPadding()
+                            .padding(innerPadding)
+                    )
                 }
             }
         }
